e318c03ec3e2096f396cd7403edf5e3a4562fdc8
bavinclint <bavinclint@gmial.com>
2022-06-08 06:34:03 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * @c: first parameter
+ * Return: last digit
+ */
+int print_last_digit(int c)
+{
+	if (c < 0)
+	{
+		c = c % 10;
+		c = -c;
+	}
+	if (c > 0)
+		c = c % 10;
+	_putchar('0' + c);
+	return (c);
+}
