94ebb3c128c39245c14cead21c716c8c641607cb
bavinclint <bavinclint@gmial.com>
2022-06-09 09:09:48 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: first parameter
+ */
+void print_triangle(int size)
+{
+	int a;
+	int b;
+
+	if (size > 0)
+	{
+		for (a = 1; a <= size; a++)
+		{
+			for (b = size; b >= 1; b--)
+			{
+				if (b > a)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
