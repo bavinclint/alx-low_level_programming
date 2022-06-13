8ecea323f5757ffbb294c8904e77554f75be3dd0
bavinclint <bavinclint@gmial.com>
2022-06-13 06:02:53 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "2-strlen.c"
 
 /**
  * _strcpy - copies the string
@@ -11,7 +12,7 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0'; i++)
+	for (i = 0; i <= _strlen(src); i++)
 	{
 		dest[i] = src[i];
 	}
