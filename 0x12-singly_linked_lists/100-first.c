df7de6051f53fbffd253d21239a96ff4776d7f76
bavinclint <bavinclint@gmial.com>
2022-07-14 10:38:06 -0700

    updated file

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * premain - run before main
+ * turtle - run before main
  */
-void __attribute__ ((constructor)) premain()
+
+void __attribute__ ((constructor)) turtle()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
 }
