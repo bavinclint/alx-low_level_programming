1b8c9f61539f9093bbdd3d96846ec865281cc39b
bavinclint <bavinclint@gmial.com>
2022-07-26 12:27:30 -0700

    header files

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,29 +1,30 @@
 #include "main.h"
-
-
 /**
- * append_text_to_file - appends text to the end of the file
- * @filename: pointer to a file
- * @text_content: string
- *
- * Return: 1 or -1
+ * append_text_to_file - appends to file
+ * @filename: name of file
+ * @text_content: content of text
+ * Return: int value
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int rd, wr, l;
+	int fd, len = 0;
+	long int wrote;
 
 	if (filename == NULL)
 		return (-1);
-	rd = open(filename, O_WRONLY | O_APPEND);
-	if (rd == -1)
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (fd == -1)
 		return (-1);
-	if (text_content == NULL)
-		return (1);
-	while (text_content[l])
-		l++;
-	wr = write(rd, text_content, l);
-	if (wr == -1)
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+
+		wrote = write(fd, text_content, len);
+		if (wrote == -1)
+			return (-1);
+	}
+	if (close(fd) == -1)
 		return (-1);
-	close(rd);
 	return (1);
 }
