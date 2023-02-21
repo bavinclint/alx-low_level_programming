4c36451ab7e6d2723850833257875beb24f9ab97
bavinclint <bavinclint@gmail.com>
2023-02-21 23:00:41 +0300

    updated file

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,33 +1,44 @@
+#include "main.h"
 #include <stdlib.h>
-
 /**
- * argstostr - concatenates all the arguements
- * @ac: argc
- * @av: argv
- *
- * Return: pointer to a string or NULL
+ * strLen - checks the length of a string
+ * @s: the string
+ * Return: the length
+ */
+int strLen(char *s)
+{
+	int size;
+
+	for (size = 0; s[size] != '\0'; size++)
+	;
+	return (size);
+}
+/**
+ * argstostr - concatenates all the arguments
+ * of the program
+ * @ac: int
+ * @av: arguments
+ * Return: a string
  */
 char *argstostr(int ac, char **av)
 {
-	int a, b, c = 0, size;
-	char *p;
+	int j, k = 0, l = 0, cmp = 0;
+	char *str;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	for (a = 0; a < ac; a++)
-	{
-		for (b = 0; av[a][b]; b++)
-			size++;
-	}
-	size += ac + 1;
-	p = malloc(size * sizeof(char));
-	if (p == NULL)
+	for (j = 0; j < ac; j++, k++)
+		k += strLen(av[j]);
+	str = malloc(sizeof(char) * k + 1);
+	if (str == 0)
 		return (NULL);
-	for (a = 0; a < ac; a++)
+	for (j = 0; j < ac; j++)
 	{
-		for (b = 0; av[a][b]; b++)
-			p[c++] = av[a][b];
-		p[c++] = '\n';
+		for (l = 0; av[j][l] != '\0'; l++, cmp++)
+			str[cmp] = av[j][l];
+		str[cmp] = '\n';
+		cmp++;
 	}
-	return (p);
+	str[cmp] = '\0';
+	return (str);
 }
