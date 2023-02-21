b00aaf27a4ca36476c074e30e512c0af0bfc89f6
bavinclint <bavinclint@gmail.com>
2023-02-22 00:39:36 +0300

    updated file

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,90 +1,251 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <main.h>
 
 /**
- * _strlen - returns the length of a string
- * @s: string
+ * _isNull - check if string is nullable
  *
- * Return: length of string
- */
-int _strlen(char *s)
-{
-	int length = 0;
-
-	while (*s++)
-		length++;
-	return (length);
-}
-
-/**
- * _puts - prints a string followed by a new line
- * @str: string
+ * @s: string to verify
  *
- * Return: void
+ * Return: 1 if string is null.
  */
-void _puts(char *str)
+int _isNull(char *s)
 {
-	while (*str)
-		_putchar(*str++);
-	_putchar('\n');
-}
-
-/**
- * _isnum - determines whether character is numerical
- * @c: character to be checked
- *
- * Return: 1 if c is numerical, 0 otherwise
- */
-int _isnum(int c)
-{
-	if (c >= '0' && c <= '9')
+	if (*s == '\0')
+	{
 		return (1);
-	else
+	}
+	if (*s != '0')
+	{
 		return (0);
+	}
+	else
+	{
+		return (_isNull(s + 1));
+	}
 }
 
 /**
- * str_isnum - determines whether a string is all numerical
- * s: string to be checked
+ * _isNumber - check if string is a number
  *
- * Return: 1 if s is all numerical, 0 otherwise
+ * @s: string to verify
+ *
+ * Return: 1 if string is a number.
  */
-int str_isnum(char *s)
+int _isNumber(char *s)
 {
-	int len = _strlen(s);
-	int i;
-
-	for (i = 0; i < len; i++)
-		if (!(_isnum(s[i])))
-			return (0);
-	return (1);
+	if (*s == '\0')
+	{
+		return (1);
+	}
+	if (!isdigit(*s))
+	{
+		return (0);
+	}
+	else
+	{
+		return (_isNumber(s + 1));
+	}
 }
 
+/**
+ * _strlen_recursion - return the length of a string
+ *
+ * @s: char pointer
+ *
+ * Return: the length of a string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (_strlen_recursion(s + 1) + 1);
+	}
+	else
+	{
+		return (0);
+	}
+}
 
 /**
- * main - multiply two positive numbers and print the answer
- * @argc: argument count
- * @argv: arguments
+ * _memset - fills the first n bytes of the memory area pointed to by s
+ *           with the constant byte b
  *
- * Return: 0 for success, other for failure
+ * @s: pointer to memory area
+ * @b: constant
+ * @n: number ot bytes to fill
+ *
+ * Return: write n bytes of value b
  */
-int main(int argc, char **argv)
+int *_memset(int *s, int b, unsigned int n)
 {
-	unsigned int len_top, len_bot;
+	unsigned int cLoop;
+
+	for (cLoop = 0; cLoop < n; cLoop++)
+	{
+		s[cLoop] = b;
+	}
+
+	return (s);
+}
+
+/**
+ * _intermediate_multiply - multiplication by the number 2
+ *
+ * @prmResult: array of the current result
+ * @prmCLoop: current index loop of the number 1
+ * @prmDigit: current digit to multiply
+ * @prmNum: Number 2 to multiply
+ *
+ * Return: integer number
+ */
+int *_intermediate_multiply(
+	int *prmResult,
+	int prmCLoop,
+	int prmDigit,
+	char *prmNum
+) {
+	int carry = 0, cLoop2 = 0, size = _strlen_recursion(prmNum), indexNumber;
+	int digit, sum = 0;
+
+	for (indexNumber = size - 1; indexNumber >= 0; indexNumber--)
+	{
+		digit = prmNum[indexNumber] - 48;
+		sum = prmDigit * digit + prmResult[prmCLoop + cLoop2] + carry;
+		carry = sum / 10;
+		prmResult[prmCLoop + cLoop2] = sum % 10;
+		cLoop2++;
+	}
+
+	if (carry > 0)
+	{
+		prmResult[prmCLoop + cLoop2] += carry;
+	}
+
+	return (prmResult);
+}
+
+/**
+ * _revInt - create a reverted array of the result
+ *
+ * @prmNum: array result
+ * @prmSize: size of the result
+ *
+ * Return: character array
+ */
+char *_revInt(int *prmNum, int prmSize)
+{
+	int digit, indexNumber1, indexNumber2;
+	char *s;
+
+	indexNumber1 = prmSize - 1;
+	s = malloc(sizeof(char) * (prmSize + 1));
+
+	for (indexNumber2 = 0; indexNumber2 < prmSize; indexNumber2++)
+	{
+		digit = prmNum[indexNumber1 - indexNumber2];
+		s[indexNumber2] = digit + 48;
+	}
+
+	s[prmSize] = '\0';
+
+	return (s);
+}
+
+/**
+ * _multiply - multiplies two positive numbers.
+ *
+ * @prmNum1: char number 1
+ * @prmNum2: char number 2
+ *
+ * Return: result.
+ */
+char *_multiply(char *prmNum1, char *prmNum2)
+{
+	int size1 = _strlen_recursion(prmNum1);
+	int size2 = _strlen_recursion(prmNum2);
+	int size = size1 + size2;
+	int *result = malloc(sizeof(int) * size);
+	int cLoop1 = 0, indexNumber1, digit1;
+	char *s;
+
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+
+	if (size1 == 0 || size2 == 0)
+	{
+		return (NULL);
+	}
+
+	result = _memset(result, 0, size1 + size2);
+
+	for (indexNumber1 = size1 - 1; indexNumber1 >= 0; indexNumber1--)
+	{
+		digit1 = prmNum1[indexNumber1] - 48;
+
+		result = _intermediate_multiply(result, cLoop1, digit1, prmNum2);
+
+		cLoop1++;
+	}
+
+	for (
+		indexNumber1 = size - 1;
+		indexNumber1 >= 0 && result[indexNumber1] == 0;
+		indexNumber1--
+	) {
+		if (result[indexNumber1] != 0)
+			break;
+	}
+
+	if (indexNumber1 == -1)
+	{
+		return (NULL);
+	}
+
+	size = indexNumber1 + 1;
+
+	s = _revInt(result, indexNumber1 + 1);
+
+	return (s);
+}
+
+/**
+ * main - check the code for number of arguments.
+ *
+ * @argc: number of arguments
+ * @argv: argument's array
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int cLoop, hasNullableArgument;
 
 	if (argc != 3)
 	{
-		_puts("Error");
-		exit(98);
+		printf("Error\n");
+		return (98);
 	}
 
-	if ((!(str_isnum(argv[1]))) || (!(str_isnum(argv[2]))))
+	for (cLoop = 1; cLoop < argc; cLoop++)
 	{
-		_puts("Error");
-		exit(98);
+		if (!_isNumber(argv[cLoop]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+
+		if (!hasNullableArgument && _isNull(argv[cLoop]))
+			hasNullableArgument = 1;
 	}
 
-	len_top = _strlen(argv[1]);
-	len_bot = _strlen(argv[2]);
-	return (p);
+	if (hasNullableArgument)
+		printf("%s\n", "0");
+	else
+		printf("%s\n", _multiply(argv[1], argv[2]));
+
+	return (0);
 }
