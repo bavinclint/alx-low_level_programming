98a7d64fd2787b329fe27d4d2947a2c41352ee29
bavinclint <bavinclint@gmail.com>
2023-02-21 10:56:20 +0300

    updated

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,1 +1,23 @@
-abc123
+#include <stdio.h>
+#include<stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ * Description: generates random valid passwords
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	char b;
+
+	srand(time(NULL));
+	while (a <= 2645)
+	{
+		b = rand() % 128;
+		a += b;
+		putchar(b);
+	}
+	putchar(2772 - a);
+	return (0);
+}
