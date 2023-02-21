7c0c7eeb7a7ad5246d9731f14c83496458bb5db0
bavinclint <bavinclint@gmail.com>
2023-02-22 00:29:03 +0300

    updated file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,12 +14,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
 	unsigned int x, y, z;
-	
+
 	if (s1 == 0)
 		s1 = "";
 	if (s2 == 0)
 		s2 = "";
-	
+
 	for (x = 0; s1[x]; x++)
 		;
 	for (y = 0; s2[y]; y++)
